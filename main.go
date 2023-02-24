9f04c3ff459e5c0bf0a09d9001b74b3766775f6b
沈长青 <changqing.shen@tenclass.com>
2023-02-24 11:40:33 +0800

    fix health_check

--- main.go
+++ main.go
@@ -24,11 +24,6 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	// add pod healthCheck
-	mux.HandleFunc("/health_check", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	})
-
 	// exec deploy pod name renew
 	mux.HandleFunc("/mutation", deploy.AddAnno)
 
@@ -38,6 +33,23 @@ func main() {
 		ReadTimeout: 20 * time.Second, WriteTimeout: 20 * time.Second,
 	}
 
+	// add healthCheck
+	go func() {
+		healthCheck()
+	}()
+
 	log.Fatal(server.ListenAndServeTLS(tlsCrt, tlsKey))
 
+}
+
+func healthCheck() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health_check", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+	server := &http.Server{
+		Addr:    ":8081",
+		Handler: mux,
+	}
+	log.Fatal(server.ListenAndServe())
 }
